a297770b6a9c8e089a4f06d60674f5b38b0af683
Michal Drobňák <mi.drobnak@gmail.com>
2023-10-12 17:42:15 +0200

    style(prettier): add prettier & format

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { ShongoCalendarModule } from '@michaldrobnak/shongo-calendar';
+import { ShongoCalendarModule } from '@CESNET/shongo-calendar';
 import { MOMENT } from 'angular-calendar';
 import moment from 'moment';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CalendarHeaderComponent } from './components/calendar-header/calendar-header.component';
-import {MatToolbarModule } from "@angular/material/toolbar";
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { CalendarComponent } from './components/calendar/calendar.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    CalendarHeaderComponent,
-    CalendarComponent
-  ],
+  declarations: [AppComponent, CalendarHeaderComponent, CalendarComponent],
   imports: [
     BrowserModule,
     ShongoCalendarModule.forRoot(),
     BrowserAnimationsModule,
-    MatToolbarModule
+    MatToolbarModule,
   ],
   providers: [
     {
@@ -27,6 +23,6 @@ import { CalendarComponent } from './components/calendar/calendar.component';
       useValue: moment,
     },
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
