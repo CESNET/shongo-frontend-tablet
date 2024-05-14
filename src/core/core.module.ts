d57051ecfa0bc514b4f0763373e4c4c52bc3f47d
Michal Drobňák <mi.drobnak@gmail.com>
2024-05-14 22:26:18 +0200

    feat: localise angular date pipe

--- src/core/core.module.ts
+++ src/core/core.module.ts
@@ -1,8 +1,11 @@
-import { CommonModule } from '@angular/common';
+import { CommonModule, registerLocaleData } from '@angular/common';
+import cs from '@angular/common/locales/cs';
 import { NgModule, Optional, SkipSelf } from '@angular/core';
 import { MOMENT } from 'angular-calendar';
 import moment from 'moment';
 import { EnsureModuleLoadedOnceGuard } from './ensure-module-loaded-once.guard';
+
+registerLocaleData(cs);
 
 @NgModule({
   declarations: [],
