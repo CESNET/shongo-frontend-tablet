933b0387f83dfb27fba5674dde6b8fcac37d8ca0
Michal Drobňák <mi.drobnak@gmail.com>
2024-05-14 22:01:15 +0200

    feat: moment locale setting

--- src/app/services/i18n.service.ts
+++ src/app/services/i18n.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, computed, signal } from '@angular/core';
 import { DEFAULT_LOCALE } from '@app/models/constants';
 import { ILocaleItem } from '@app/models/interfaces';
+import * as moment from 'moment';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +10,12 @@ export class I18nService {
   selectedLocaleSig = signal<ILocaleItem>(DEFAULT_LOCALE);
   selectedLocaleValueSig = computed(() => this.selectedLocaleSig().value);
 
+  constructor() {
+    moment.locale(DEFAULT_LOCALE.value);
+  }
+
   selectLocale(locale: ILocaleItem): void {
     this.selectedLocaleSig.set(locale);
+    moment.locale(locale.value);
   }
 }
